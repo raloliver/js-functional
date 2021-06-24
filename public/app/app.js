f4750cf77ab120503ca9bd19d0be45714a01344a
raloliver <contato@raloliver.com.br>
2021-06-24 13:07:31 +0100

    closure example

--- public/app/app.js
+++ public/app/app.js
@@ -1,13 +1,20 @@
 import { handleStatus, log } from "../utils/util-helpers.js";
 import "../utils/list-helpers.js";
 
+/**
+ * closure: capacidade que uma função tem de lembrar do contexto ao qual ela foi declarada
+ * e retorna uma nova função que recebe uma lista
+ * @param {*} code
+ */
+const sumItems = code => list => list
+    .$flatMap(list => list.itens)
+    .filter(item => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
+
 document.querySelector('#bigBlueButton').onclick = () => {
     fetch('/notas')
         .then(handleStatus)
-        .then(list => list
-            .$flatMap(list => list.itens)
-            .filter(item => item.codigo == '2143')
-            .reduce((total, item) => total + item.valor, 0))
+        .then(sumItems('2143'))
         .then(log)
         .catch(log);
 };
